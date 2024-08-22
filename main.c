d8c22fbe4733ed9b3655e1c80bdcfa552ea4965a
Haim Ozer <haim3x@gmail.com>
2024-08-22 10:55:23 +0300

    Simplefy code and tests

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+// Haim Ozer
+// Id: 316063569 
+// RETD - C Language For Embedded: Final project - BlackJack game 
+// Date: 18/08/2024 ~ 20/9/2024 
+// Lecturer: Shmuel 
+
 #include "game.h"
 
 int main() {
